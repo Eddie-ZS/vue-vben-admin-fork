0e15901b67edb20fd5e0ff09ded714aea02308bb
xiaoyuan-zs-com <lcshuxiaoluo@163.com>
2024-11-12 17:04:46 +0800

    feat: 完成unbuild构建

---
+++ internal/lint-configs/eslint-config/build.config.ts
@@ -0,0 +1,8 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  clean: true, // clean dist directory before build
+  entries: ['./src/index.ts'], // entry file
+  declaration: true, // generate.d.ts file
+  // outDir: "dist"  // Change outDir, default is 'dist'
+})
