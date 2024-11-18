e2bfd5c6f8217d528759a672bfd6691e402fcf3d
xiaoyuan-zs-com <lcshuxiaoluo@163.com>
2024-11-18 17:31:26 +0800

    fix: 修改

--- internal/lint-configs/commitlint-config/src/index.mjs
+++ internal/lint-configs/commitlint-config/src/index.mjs
@@ -8,56 +8,50 @@ const allowScopedPackages = [...packages.map((pkg) => pkg.packageJson.name), 'ci
 const userConfig = {
 	extends: ['@commitlint/config-conventional'],
 	plugins: ['commitlint-plugin-function-rules'], // 使用函数作为规则值。
+	/***
+	 * 规则配置
+	 * 0：禁用规则 1：警告 2：错误
+	 * always：某项规则必须满足，不满足则会触发错误或警告  -> 是
+	 * never：某些规则不能被满足，满足则会触发错误或警告 -> 否
+	 */
 	rules: {
 		/**
-		 *  condition: 提交的body以value结尾
-		 *  rule: never 默认为never，提交信息不以.结尾
-		 *  value: 默认值.
+		 * condition: body 以空行开头
 		 */
 		'body-leading-blank': [2, 'always'],
 
 		/**
-		 * condition: footer 是否以空行开始
-		 * rule: always
+		 * condition: footer 以空行开始
 		 */
 		'footer-leading-blank': [1, 'always'],
 
 		/**
-		 * condition: header 最大长度
-		 * rule: always
-		 * value: 72 默认72个字符
+		 * condition: 标头 header 最大长度
+		 * value: 72 默认72个字符 不超过72个字符
 		 */
 		'header-max-length': [2, 'always', 108],
 
 		/**
-		 * condition: 影响范围scope 的枚举，提交信息时只能从中选择
-		 * rule: always
-		 * value: []
+		 * condition: 关闭默认的，使用插件函数的scope-enum
 		 */
 		'scope-enum': [0],
 
 		/**
-		 * description: type 的输入格式
-		 * rule: always
-		 * value:'lower-case'，默认为小写，可选列表同上
+		 * description: 忽略提交信息主体的大小写
 		 */
 		'subject-case': [0],
 
 		/**
-		 * condition: subject 是否为空
-		 * rule: never
+		 * condition: subject 不能为空
 		 */
 		'subject-empty': [2, 'never'],
 
 		/**
-		 * condition: type 是否可为空
-		 * rule: never
+		 * condition: type 不能为空
 		 */
 		'type-empty': [2, 'never'],
 		/**
 		 * condition: 影响范围scope 的枚举，提交信息时只能从中选择
-		 * rule: always
-		 * value: []
 		 */
 		'function-rules/scope-enum': [
 			2, // level: error
@@ -71,11 +65,12 @@ const userConfig = {
 			}
 		],
 
-		// 类型枚举，git提交type必须是以下类型
+		/**
+		 * 类型枚举，git提交type必须是以下类型
+		 */
 		'type-enum': [
 			// 当前验证的错误级别
 			2,
-			// 在什么情况下进行验证，always表示一直进行验证
 			'always',
 			[
 				'feat', // 新增功能
@@ -88,10 +83,38 @@ const userConfig = {
 				'build', // 构建流程、外部依赖变更（如升级 npm 包、修改 webpack 配置等）
 				'ci', // 修改 CI 配置、脚本
 				'revert', // 回滚 commit
-				'chore' // 对构建过程或辅助工具和库的更改（不影响源文件、测试用例）
+				'chore', // 对构建过程或辅助工具和库的更改（不影响源文件、测试用例）
+				'types' // 类型定义文件修改
 			]
 		]
 	},
-	prompt: {}
+	prompt: {
+		messages: {
+			type: '选择你要提交的类型 :',
+			scope: '选择一个提交范围（可选）:',
+			customScope: '请输入自定义的提交范围 :',
+			subject: '填写简短精炼的变更描述 :\n',
+			body: '填写更加详细的变更描述（可选）。使用 "|" 换行 :\n',
+			breaking: '列举非兼容性重大的变更（可选）。使用 "|" 换行 :\n',
+			footerPrefixesSelect: '选择关联issue前缀（可选）:',
+			customFooterPrefix: '输入自定义issue前缀 :',
+			footer: '列举关联issue (可选) 例如: #31, #I3244 :\n',
+			confirmCommit: '是否提交或修改commit ?'
+		},
+		types: [
+			{ value: 'feat', name: 'feat:     新增功能 | A new feature' },
+			{ value: 'fix', name: 'fix:      修复缺陷 | A bug fix' },
+			{ value: 'docs', name: 'docs:     文档更新 | Documentation only changes' },
+			{ value: 'style', name: 'style:    代码格式 | Changes that do not affect the meaning of the code' },
+			{ value: 'refactor', name: 'refactor: 代码重构 | A code change that neither fixes a bug nor adds a feature' },
+			{ value: 'perf', name: 'perf:     性能提升 | A code change that improves performance' },
+			{ value: 'test', name: 'test:     测试相关 | Adding missing tests or correcting existing tests' },
+			{ value: 'build', name: 'build:    构建相关 | Changes that affect the build system or external dependencies' },
+			{ value: 'ci', name: 'ci:       持续集成 | Changes to our CI configuration files and scripts' },
+			{ value: 'revert', name: 'revert:   回退代码 | Revert to a commit' },
+			{ value: 'chore', name: 'chore:    其他修改 | Other changes that do not modify src or test files' },
+			{ value: 'types', name: 'type: 类型定义文件修改 | ' }
+		]
+	}
 };
 export default userConfig;
