2e5f3a3fa6c380991e33715db77e94d72349f639
xiaoyuan-zs-com <lcshuxiaoluo@163.com>
2024-11-27 15:13:43 +0800

    feat(@vbird/node-utils): perfect notd-utils module function

--- internal/node-utils/src/index.ts
+++ internal/node-utils/src/index.ts
@@ -1,4 +1,21 @@
+export * from './constants';
 export * from './monorepo';
+export * from './fs';
+export * from './date';
+export * from './git';
+export * from 'execa';
+export { add as gitAdd, getStagedFiles } from './git';
+export { generateContentHash } from './hash';
+export { toPosixPath } from './path';
+export { prettierFormat } from './prettier';
 
+export type { Package } from '@manypkg/get-packages';
+// 优雅的控制台包装器
+export { consola } from 'consola';
+// 终端字符串样式
+export { default as colors } from 'chalk';
 // 基于 promise 的 文件系统
 export { default as fs } from 'node:fs/promises';
+
+export { type PackageJson, readPackageJSON } from 'pkg-types';
+export { rimraf } from 'rimraf';
