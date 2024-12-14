cffb44919494c6ee5baf0b27943720a44a5aeee1
Eddie <lcshuxiaoluo@163.com>
2024-12-14 15:34:55 +0800

    feat(@vbird/vite-config): init plugins methods

--- internal/vite-config/src/plugins/index.ts
+++ internal/vite-config/src/plugins/index.ts
@@ -1,1 +1,16 @@
-export const plugins = [];
+import type { PluginOption } from 'vite';
+import type { ApplicationPluginOptions, LibraryPluginOptions } from '../types';
+
+/**
+ * 根据条件获取应用类型的vite插件
+ */
+async function loadApplicationPlugins(options: ApplicationPluginOptions): Promise<PluginOption[]> {
+	return await [];
+}
+
+/**
+ * 根据条件获取库类型的vite插件
+ */
+async function loadLibraryPlugins(options: LibraryPluginOptions): Promise<PluginOption[]> {
+	return await [];
+}
