34c8d8e1ac8c49aae15245a6677d2dd04fff50e0
Eddie <lcshuxiaoluo@163.com>
2024-12-22 00:48:42 +0800

    fix(@vbird/vite-config):  初始化vite-config中默认应用配置(插件初始化)以及调整作为依赖安装配置导出

--- internal/vite-config/src/config/application.ts
+++ internal/vite-config/src/config/application.ts
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig, type UserConfig } from 'vite';
 import type { DefineApplicationOptions } from '../types';
+import { loadApplicationPlugins } from '../plugins';
+import { loadAndConvertEnv } from '../utils/env';
 
 /**
  * @description: Application configuration (应用模式 配置)
@@ -7,7 +9,20 @@ import type { DefineApplicationOptions } from '../types';
  */
 function defineApplicationConfig(userConfigPromise?: DefineApplicationOptions) {
 	return defineConfig(async (config) => {
-		return config;
+		// 加载用户自定义配置
+		const options = await userConfigPromise?.(config);
+		// 读取环境变量
+		const { base } = await loadAndConvertEnv();
+		// 加载应用插件
+		const plugins = await loadApplicationPlugins({
+			devtools: true
+		});
+
+		const applicationConfig: UserConfig = {
+			plugins
+		};
+		// 合并用户配置
+		return applicationConfig;
 	});
 }
 
