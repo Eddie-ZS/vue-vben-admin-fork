1ad64da6a5a6845e4de37b3c7b127d114f6eabac
Eddie <lcshuxiaoluo@163.com>
2024-12-24 22:02:40 +0800

    fix(@vbird/vite-config): 修复启动项目自动浏览器打开 index.html 的问题

--- internal/vite-config/src/config/index.ts
+++ internal/vite-config/src/config/index.ts
@@ -1,6 +1,6 @@
 import type { DefineConfig } from '../types';
 import { join } from 'node:path';
-import { execSync } from 'node:child_process';
+import { existsSync } from 'node:fs';
 import { defineApplicationConfig } from './application';
 import { defineLibraryConfig } from './library';
 
@@ -14,7 +14,7 @@ function defineConfig(useConfigPromise?: DefineConfig, type: 'application' | 'au
 	//  若类型为 auto, 判断根目录下是否存在 index.html, 若存在则为 application（应用）, 否则为 library（库）
 	if (projectType === 'auto') {
 		const indexPath = join(process.cwd(), 'index.html');
-		projectType = execSync(indexPath) ? 'application' : 'library';
+		projectType = existsSync(indexPath) ? 'application' : 'library';
 	}
 
 	switch (projectType) {
