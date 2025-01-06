00c38dd8914256598790f0825c7d1bc7945b2830
Eddie <lcshuxiaoluo@163.com>
2025-01-06 22:53:57 +0800

    feat: 完善 @core/base 包

---
+++ packages/@core/base/shared/src/constants/vbird.ts
@@ -0,0 +1,25 @@
+/**
+ * @zh_CN GITHUB 仓库地址
+ */
+export const VBIRD_GITHUB_URL = 'https://github.com/vbenjs/vue-vben-admin';
+
+/**
+ * @zh_CN 文档地址
+ */
+export const VBIRD_DOC_URL = 'https://doc.vben.pro';
+
+/**
+ * @zh_CN Vbird Logo
+ */
+export const VBIRD_LOGO_URL = 'https://unpkg.com/@vbenjs/static-source@0.1.7/source/logo-v1.webp';
+
+/**
+ * @zh_CN Vbird Admin 首页地址
+ */
+export const VBIRD_PREVIEW_URL = 'https://www.vben.pro';
+
+export const VBIRD_ELE_PREVIEW_URL = 'https://ele.vben.pro';
+
+export const VBIRD_NAIVE_PREVIEW_URL = 'https://naive.vben.pro';
+
+export const VBIRD_ANT_PREVIEW_URL = 'https://ant.vben.pro';
