b6927df579726b9705885aac55465f42a8f67ef2
Eddie <lcshuxiaoluo@163.com>
2025-01-14 21:02:50 +0800

    feat(@vbird/turbo-run): 完善 turbo-run 交互式项目启动脚本

--- scripts/turbo-run/src/run.ts
+++ scripts/turbo-run/src/run.ts
@@ -1,6 +1,6 @@
-import { getPackagesAsync } from '@vbird/node-utils';
+import { consola, execaCommand, getPackagesAsync } from '@vbird/node-utils';
 
-import { select } from '@clack/prompts';
+import { isCancel, select } from '@clack/prompts';
 
 interface RunOptions {
 	command?: string;
@@ -12,22 +12,39 @@ interface RunOptions {
 export async function run({ command }: RunOptions) {
 	if (!command) {
 		console.error('Please specify the command to run!');
-		process.exit(1);
+		process.exit(0);
 	}
-	// 获取所有包中的 package.json 文件，过滤出有 command 命令的包，然后执行相应的命令
+	// 获取所有包中的 package.json 文件，过滤出有 command 命令的包
 	const { packages } = await getPackagesAsync();
 	const packagesWithCommand = packages.filter((pkg) => (pkg.packageJson as Record<string, any>)?.scripts?.[command]);
 
-	let isSelectPkg: string | symbol;
+	let selectAppName: string | symbol;
 	if (packagesWithCommand.length > 1) {
 		// 单选需要运行的包
-		isSelectPkg = await select({
+		selectAppName = await select({
 			message: `Please select the application you want to run [${command}]:`,
+			// select 组件允许用户从选项列表中选择一个值。结果是给定选项的 value 属性
 			options: packagesWithCommand.map((el) => ({
 				label: el.packageJson.name,
 				value: el.packageJson.name
 			}))
 		});
-		// if (isCancel(isSelectPkg)) {}
+		// 按 ctrl+c 正常退出
+		if (isCancel(selectAppName) || !selectAppName) {
+			consola.info('Process exit!');
+			process.exit(0);
+		}
+	} else {
+		selectAppName = packagesWithCommand[0]?.packageJson.name ?? '';
 	}
+	// 若不存在可运行的包，则提示用户
+	if (!selectAppName) {
+		consola.info('No Found Application!');
+		process.exit(0);
+	}
+
+	// 执行命令
+	execaCommand(`pnpm -F ${selectAppName} run ${command}`, {
+		stdio: 'inherit'
+	});
 }
