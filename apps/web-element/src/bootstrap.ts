bb4a18ac55cc593f5b7dce9fb5528dc5dc678110
Eddie <lcshuxiaoluo@163.com>
2025-01-15 23:31:29 +0800

    feat(@vbird/web-element): 增加应用 web-element 入口文件功能 (待完善 vite scss 全局样式注入 additionalData 不生效问题)

--- apps/web-element/src/bootstrap.ts
+++ apps/web-element/src/bootstrap.ts
@@ -1,1 +1,14 @@
-export function bootstrap() {}
+import { createApp } from 'vue';
+
+// 导入css样式
+import '@vbird/styles';
+// 导入自定义 element-plus 样式文件
+import '@vbird/styles/ele';
+
+import App from './App.vue';
+
+export async function bootstrap(namespace: string) {
+	const app = createApp(App);
+
+	app.mount('#app');
+}
--- apps/web-element/src/main.ts
+++ apps/web-element/src/main.ts
@@ -1,7 +1,14 @@
-import { createApp } from 'vue';
+/**
+ * 应用初始化完成之后再进行页面加载渲染
+ */
+async function initApplication() {
+	// 用于区分不同项目的偏好设置以及存储数据的key前缀以及其他一些需要隔离的数据
+	const env = import.meta.env.PROD ? 'prod' : 'dev';
+	const appVersion = import.meta.env.VITE_APP_VERSION;
+	const namespace = `${import.meta.env.VITE_APP_NAMESPACE}-${appVersion}-${env}`;
 
-import App from './App.vue';
+	const { bootstrap } = await import('./bootstrap');
+	await bootstrap(namespace);
+}
 
-const app = createApp(App);
-
-app.mount('#app');
+initApplication();
