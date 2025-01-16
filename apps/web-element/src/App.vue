f4f1ada2ba3097439bfb106c11530dd66900cf9e
xiaoyuan-zs-com <lcshuxiaoluo@163.com>
2025-01-16 17:27:44 +0800

    test: 测试样式BEM 规范

--- apps/web-element/src/App.vue
+++ apps/web-element/src/App.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';
 
+import { useNamespace } from '@vbird/hooks';
+
+const { b, e } = useNamespace('menu');
+
 onMounted(() => {});
 </script>
 
 <template>
-	<div>123</div>
+	<div :class="[b()]">
+		123
+		<div :class="[e('item')]">456</div>
+	</div>
 </template>
+
+<style lang="scss" scoped>
+@include b('menu') {
+	color: red;
+
+	@include e('item') {
+		color: orange;
+	}
+}
+</style>
